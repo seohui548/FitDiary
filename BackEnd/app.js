aae21a9b6479c49beb96a0d410c970809f860644
Sang min Lee <kingdog6694@gmail.com>
2021-11-13 00:58:28 -0500

    Initiate homepage template

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require("path");
 
 const app = express()
 
+app.set("view engine", "ejs");
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 app.get('/', (req, res) => {
-    res.send('상민님 만나서 반갑습니다. 짧은 시간이지만 화이팅해요! :)');
+    // res.redner('상민님 만나서 반갑습니다. 짧은 시간이지만 화이팅해요! :)');
+    res.render("home");
 })
 
 app.get('/api/user', (req, res) => {
