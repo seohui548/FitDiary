629ecf9933bfa980176635cec53a9bdf1c5db59b
Sang min Lee <kingdog6694@gmail.com>
2021-11-14 03:51:22 -0500

    Remove activity feature added

--- BackEnd/public/scripts/ticket-popup.js
+++ BackEnd/public/scripts/ticket-popup.js
@@ -9,9 +9,9 @@ class TicketPopup extends HTMLElement {
 
   init() {
     this.activityId = this.dataset.activityId;
-    this.addButton = this.querySelector(".ticket-btn");
+    this.featureButton = this.querySelector(".ticket-btn");
     this.addEventListener("click", this.popupClick.bind(this));
-    this.addButton.addEventListener("click", this.addActivity.bind(this));
+    if(this.featureButton) this.featureButton.addEventListener("click", this.actionChoice.bind(this));
   }
 
   popupClick(event) {
@@ -39,11 +39,14 @@ class TicketPopup extends HTMLElement {
             <p>${date}</p>
             <p>${address}</p>
             <p>${current_capacity}/${max_capacity}</p>
+            ${eventExists ? `<p class="disclaimer">You are registered to this event!</p>` : ""}
           </div>
           <div class="ticket-gradient"></div>
         </div>
         <div class="ticket__action">
-          <button class="ticket-btn" ${eventExists ? "disabled" : ""}>Add to my schedule</button>
+          ${eventExists ? `<button class="ticket-btn" data-feature-type="remove">Remove Activity</button>` :
+            `<button class="ticket-btn" data-feature-type="add">Add Activity</button>`
+          }
         </div>
       </div>
     `;
@@ -53,16 +56,22 @@ class TicketPopup extends HTMLElement {
     this.init();
   }
 
-  addActivity() {
+  actionChoice() {
     const chosenEvent = window.activities[this.diaryBook.activeBtn.dataset.fitCategory]
                         .filter((activity) => activity.id === this.activityId)[0];
-    this.addActivityToSchedule(chosenEvent);
-    chosenEvent.current_capacity += 1;
-    alert(`The activity "${chosenEvent.title}" has been added to your schedule!`);
-    this.closePopup();
+
+    const featureType = this.featureButton.dataset.featureType;
+    switch(featureType) {
+      case "add":
+        this.addActivity(chosenEvent);
+        break;
+      case "remove":
+        this.removeActivity(chosenEvent);
+        break;
+    }
   }
 
-  addActivityToSchedule(chosenEvent) {
+  addActivity(chosenEvent) {
     const eventToAdd = {
       activity_id: chosenEvent.id,
       activity_title: chosenEvent.title,
@@ -80,6 +89,24 @@ class TicketPopup extends HTMLElement {
       const myEvents = JSON.parse(localStorage.getItem("myEvents"));
       myEvents.push(eventToAdd);
       localStorage.setItem("myEvents", JSON.stringify(myEvents));
+    }
+
+    chosenEvent.current_capacity += 1;
+    alert(`The activity "${chosenEvent.title}" has been added to your schedule!`);
+    this.closePopup();
+  }
+
+  removeActivity(chosenEvent) {
+    if(localStorage.getItem("myEvents")) {
+      const myEvents = JSON.parse(localStorage.getItem("myEvents"));
+      const updatedEvents = myEvents.filter((evt) => {
+        return evt.activity_id !== chosenEvent.id;
+      });
+
+      localStorage.setItem("myEvents", JSON.stringify(updatedEvents));
+      chosenEvent.current_capacity -= 1;
+      alert(`The activity "${chosenEvent.title}" has been removed from your schedule!`);
+      this.closePopup();
     }
   }
 }
